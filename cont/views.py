1423b7aa28d67b7ac8d9eb1b692c73167c9cd6c2
jguaraz <jguaraz@openmailbox.org>
2016-02-24 13:39:50 -0300

    Homecont version alpha - new factor commit

---
+++ cont/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.utils import timezone
+from .models import SumasYSaldos
+
+# Create your views here.
+def base(request):
+    return render(request, 'pages/base.html', {})
+def front(request):
+    return render(request, 'pages/front.html', {})
+
+def balance0(request):
+    #balance = SumasYSaldos.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+
+	result = SumasYSaldos.objects.raw('SELECT * FROM sumas_y_saldos WHERE debe > 0 or haber > 0')
+	#result = SumasYSaldos.objects.all()
+	return render(request, 'pages/balance0.html', {'results': result})
---
+++ myvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/juangu/django/projects/homecont/myvenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
