d30d1ad9d828ca66cf3f709b17ff1d4f7a6e1946
jguaraz <jguaraz@openmailbox.org>
2016-02-27 12:17:58 -0300

    Cleanup made

--- mysite/settings.py
+++ mysite/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'homecnt_test.db'),
+        'NAME': os.path.join(BASE_DIR, 'homecnt_2016.db'),
     }
 }
 
